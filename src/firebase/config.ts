d4b13be0cda6099d8c5698056faedf456ea06365
Peter Damiano <peterleodamiano@gmail.com>
2025-10-29 12:36:41 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6001516789-8dae3",
+  "appId": "1:1095630329173:web:99fe467eac69c58b540bdc",
+  "apiKey": "AIzaSyCPtrORe3rmnL5Jrk2jyfwrN8IY-1TX7CM",
+  "authDomain": "studio-6001516789-8dae3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1095630329173"
+};
