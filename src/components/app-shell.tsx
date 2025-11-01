69226145a0653023ccf2a96329568d3f4d3ae2ec
Peter Damiano <peterdamianotech@gmail.com>
2025-11-01 14:41:03 +0000

    Make students dashboard fixed on screen size no scrolling or zooming of

--- src/components/app-shell.tsx
+++ src/components/app-shell.tsx
@@ -6,7 +6,7 @@ import { AppHeader } from "./app-header";
 export function AppShell({ children }: { children: React.ReactNode }) {
     return (
         <SidebarProvider>
-            <div className="flex h-svh flex-col">
+            <div className="flex h-svh flex-col min-w-0">
               <AppHeader />
               <main className="flex-1 overflow-y-auto p-4 md:p-6 lg:p-8">
                 {children}
