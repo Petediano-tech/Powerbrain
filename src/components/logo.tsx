663c3e8f6a507dd8a132f7c4f582697236b61b03
Peter Damiano <peterleodamiano@gmail.com>
2025-11-03 20:20:31 +0000

    Ok use this image URL for my app logo https://i.ibb.co/LXVP2DvQ/file-000

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,4 +1,4 @@
-import { BrainCircuit } from 'lucide-react';
+import Image from 'next/image';
 import { Poppins } from 'next/font/google';
 import { cn } from '@/lib/utils';
 
@@ -10,8 +10,8 @@ const logoFont = Poppins({
 export function Logo() {
   return (
     <div className="flex items-center gap-2 group-data-[collapsible=icon]:justify-center">
-      <div className="flex h-10 w-10 items-center justify-center rounded-lg bg-primary/20">
-        <BrainCircuit className="h-6 w-6 text-primary" />
+      <div className="flex h-10 w-10 items-center justify-center rounded-lg">
+        <Image src="https://i.ibb.co/LXVP2DvQ/file-00000000c054622f99a4d97070b6f180.png" alt="Power Brain Logo" width={40} height={40} className="object-contain" />
       </div>
       <div className={cn("flex flex-col group-data-[collapsible=icon]:hidden", logoFont.className)}>
         <h1 className="text-lg font-bold leading-tight text-primary">Power Brain</h1>
